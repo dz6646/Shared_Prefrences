8f022d3cd02990e80047547fa062c0e61b341227
Dan Zhuravlov <yuoirty@gmail.com>
2023-02-14 17:16:50 +0200

    the code is wokrinf

--- app/src/main/java/com/example/shared_prefrences/MainActivity.java
+++ app/src/main/java/com/example/shared_prefrences/MainActivity.java
@@ -11,8 +11,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int count = 0;
-    EditText inputName = findViewById(R.id.input_name);
-    TextView countView = findViewById(R.id.count);
+    String name;
+    EditText inputName;
+    TextView countView;
     SharedPreferences settings;
 
     @Override
@@ -20,22 +21,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         settings=getSharedPreferences("PREFS_NAME",MODE_PRIVATE);
-
-
-
+        inputName = findViewById(R.id.input_name);
+        countView = findViewById(R.id.count);
+        readShared();
     }
 
     public void saveToFile(View view) {
-        String name;
         SharedPreferences.Editor editor=settings.edit();
         editor.putInt("count", count);
         name = inputName.getText().toString();
         editor.putString("name", name);
         editor.commit();
+        finish();
     }
 
     public void progCount(View view) {
         count++;
         countView.setText("" + count);
+    }
+
+    public void resetCount(View view) {
+        count = 0;
+        countView.setText("0");
+    }
+
+    public void readShared()
+    {
+        count = settings.getInt("count", 0);
+        name = settings.getString("name", "");
+        countView.setText("" + count);
+        inputName.setText(name);
     }
 }
